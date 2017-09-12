a0e4d146540fcf4f1294c4ca1411c8f49678812b
Аня Ташлыкова <tashlykova-anna@mail.ru>
2017-09-12 15:40:56 +0500

    Добавлен класс с двунаправленным списком

---
+++ Задание_9/Задание_9/MyDoubleList.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PI_Zad9
+{
+    public class MyDoubleList
+    {
+        class ListItem
+        {
+            public int Data { get; set; }
+            public ListItem Previous { get; set; }
+            public ListItem Next { get; set; }
+        }
+
+        private ListItem first;
+        private ListItem last;
+
+        public void Add(int item)
+        {
+            ListItem li = new ListItem();
+            li.Data = item;
+
+            if (first == null)
+            {
+                first = li;
+                last = li;
+            }
+            else
+            {
+                last.Next = li;
+                li.Previous = last;
+                last = li;
+            }
+        }
+
+        public void Show()
+        {
+            ListItem beg = first;
+
+            while (beg != null)
+            {
+                Console.Write(beg.Data + " ");
+
+                beg = beg.Next;
+            }
+
+            Console.WriteLine();
+        }
+
+        public bool Find(int el)
+        {
+            ListItem beg = first;
+            bool find = false;
+
+            while (beg != null)
+            {
+                if (beg.Data == el)
+                {
+                    find = true;
+                    break;
+                }
+
+                beg = beg.Next;
+            }
+
+            return find;
+        }
+
+        public bool Delete(int el)
+        {
+            bool result = false;
+
+            if (Find(el))
+            {
+                ListItem beg = first;
+                result = true;
+
+                //если удаляем первый элемент
+                if (beg.Data == el)
+                {
+                    first = beg.Next;
+                    first.Previous = null;
+
+                    if (first == null)
+                        last = null;
+                }
+                else
+                {
+                    while (beg != null)
+                    {
+                        if (beg.Data == el)
+                        {
+                            beg.Previous.Next = beg.Next;
+                            if (beg.Next != null)
+                                beg.Next.Previous = beg.Previous;
+
+                            if (beg == last)
+                                last = beg.Previous;
+
+                            break;
+                        }
+
+                        beg = beg.Next;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Задание_9/Задание_9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PI_Zad9
+{
+    class Program
+    {
+        static MyDoubleList CreateList()
+        {
+            MyDoubleList list = new MyDoubleList();
+
+            Console.WriteLine("Введите n");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                list.Add(i);
+            }
+
+            return list;
+        }
+        static void Main(string[] args)
+        {
+            MyDoubleList list = CreateList();
+            list.Show();
+
+            bool find = list.Find(4);
+            bool res = list.Delete(1);
+
+            list.Show();
+
+            res = list.Delete(5);
+
+            list.Show();
+
+            Console.ReadLine();
+        }
+    }
+}
